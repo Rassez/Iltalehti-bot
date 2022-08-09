01882a9492a34814417f51088d6b20bce21df62e
Rasmus <50022415+TehRamsus@users.noreply.github.com>
2022-08-09 19:24:23 +0300

    Version 1.0.1 Iltalehti Bot

---
+++ main.js
@@ -0,0 +1,54 @@
+const Parser = require('rss-parser');
+const { WebhookClient } = require('discord.js');
+const Config = require('./config.json')
+const parser = new Parser();
+var SaveTime;
+
+const WebClient = new WebhookClient({ id: Config.ID, token: Config.Token });
+
+// Main function
+const CheckRSS = async() => {
+    parser.parseURL(Config.NewsURL).then((NewsFeed) => {
+        const NewsItems = NewsFeed.items[0]
+        let date = new Date().toLocaleString()
+        if (SaveTime !== NewsItems.pubDate) {
+            const Embed = {
+                color: 16711680,
+                title: NewsItems.title,
+                url: NewsItems.link,
+                author: {
+                    name: NewsItems.categories[0].replace(/\b\w/g, e => e.toUpperCase()),
+                    icon_url: Config.AvatarURL
+                },
+                description: NewsItems.content,
+                Image: {
+                    url: NewsItems.enclosure.url !== undefined ? NewsItems.enclosure.url : 'https://assets.ilcdn.fi/iltalehti_placeholder_some.png',
+                },
+                footer: {
+                    text: date,
+                    icon_url: Config.AvatarURL,
+                },
+            };
+        
+            WebClient.send({
+                username: 'Iltalehti',
+                avatarURL: Config.AvatarURL,
+                embeds: [Embed],
+            }).catch((e) => {
+                console.log(e)
+            })
+            SaveTime = NewsItems.pubDate;
+        }
+
+    }).catch((e) => {
+        console.log(e)
+    });
+}
+
+// Installation
+const Init = () => {
+    CheckRSS();
+    setInterval(CheckRSS, Config.RefreshTime * 60000)
+}
+
+Init();
